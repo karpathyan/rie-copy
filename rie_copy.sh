b43cbec2a2a1dc2d738ed1cf469dc2b1842528b5
Karpathyan <34363447+karpathyan@users.noreply.github.com>
2023-07-03 12:38:54 +0200

    Add files via upload

---
+++ rie_copy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# rie_copy.sh
+# https://github.com/karpathyan/rie_copy
+# Rajgowrav Cheenikundil, Orebro University, Sweden
+# Licence: GPL
+
+
+num_args=$#
+destination_dir="${!num_args}/"
+
+if [ $num_args -lt 2 ]; then
+    echo "Usage: ./copy_files.sh <source_file1> <source_file2> ... <destination_directory>"
+    exit 1
+fi
+
+for ((i=1; i<num_args; i++)); do
+    source_file="${!i}"
+    filename=$(basename -- "$source_file")
+    destination_path="${destination_dir}${filename}"
+
+    if [ -e "$destination_path" ]; then
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        counter=1
+
+        while [ -e "$destination_path" ]; do
+            new_filename="${filename}(${counter}).${extension}"
+            destination_path="${destination_dir}${new_filename}"
+            counter=$((counter + 1))
+        done
+    fi
+
+    cp "$source_file" "$destination_path"
+done
